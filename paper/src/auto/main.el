0d704989e64e83681c01eb384a59324cd6f73d41
Daniel Rosel <daniel@alves.world>
2025-10-22 12:42:10 +0200

    reformating

--- paper/src/auto/main.el
+++ paper/src/auto/main.el
@@ -6,12 +6,14 @@
    (setq TeX-command-extra-options
          "-file-line-error -interaction=nonstopmode")
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("report" "12pt") ("article" "12pt")))
+                     '(("report" "12pt") ("article" "12pt") ("acmart" "sigconf" "nonacm")))
    (TeX-run-style-hooks
     "latex2e"
     "preamble"
     "chapters/01-intro"
-    "article"
-    "art12"))
+    "acmart"
+    "acmart10")
+   (TeX-add-symbols
+    '("footnotetextcopyrightpermission" 1)))
  :latex)
 
