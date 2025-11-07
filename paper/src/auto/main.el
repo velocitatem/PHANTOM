e04fb99f7b6aeeb500e780f23a2e34643e7c05fe
Daniel Alves Rösel <60182044+velocitatem@users.noreply.github.com>
2025-11-07 14:39:59 +0100

    13 create outline for research paper draft (#18)
    * updated outline for paper from issue
    
    * extra paper sections and some formalization of series data
    
    * algorithms and acknowledgements

--- paper/src/auto/main.el
+++ paper/src/auto/main.el
@@ -6,11 +6,17 @@
    (setq TeX-command-extra-options
          "-file-line-error -interaction=nonstopmode")
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("report" "12pt") ("article" "12pt") ("acmart" "sigconf" "nonacm")))
+                     '(("report" "12pt") ("article" "12pt") ("acmart" "sigconf" "nonacm" "natbib=false")))
    (TeX-run-style-hooks
     "latex2e"
     "preamble"
     "chapters/01-intro"
+    "chapters/02-literature-review"
+    "chapters/03-methodology"
+    "chapters/04-results"
+    "chapters/05-discussion"
+    "chapters/06-conclusion"
+    "../build/concatenated_code"
     "acmart"
     "acmart10")
    (TeX-add-symbols
