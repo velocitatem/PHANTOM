ba8c42e30ec7a1e471fd94ad2fd6f222e7fda35c
Daniel Rosel <daniel@alves.world>
2025-11-12 11:15:09 +0100

    moved middleware to proxy according to new changes in Nextjs

--- web/src/proxy.ts
+++ web/src/proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
     const mode = process.env.STORE_MODE;
     const { pathname } = req.nextUrl;
 
