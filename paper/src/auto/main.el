2dc3dad0a5665306f0e362287e8ef47a2ca902fa
Daniel Rosel <daniel@alves.world>
2025-11-14 11:44:18 +0100

    my changes and draft

--- paper/src/auto/main.el
+++ paper/src/auto/main.el
@@ -10,15 +10,11 @@
    (TeX-run-style-hooks
     "latex2e"
     "preamble"
-    "chapters/01-intro"
-    "chapters/02-literature-review"
-    "chapters/03-methodology"
-    "chapters/04-results"
-    "chapters/05-discussion"
-    "chapters/06-conclusion"
-    "../build/concatenated_code"
     "acmart"
     "acmart10")
    (TeX-add-symbols
-    '("footnotetextcopyrightpermission" 1)))
+    '("footnotetextcopyrightpermission" 1))
+   (LaTeX-add-labels
+    "research"))
  :latex)
+
