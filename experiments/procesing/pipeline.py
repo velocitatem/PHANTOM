2661b841fca52df3cf45dd16ec16ab1450cadded
Daniel Rosel <daniel@alves.world>
2025-11-22 21:09:04 +0100

    minor pipeline refactor

--- experiments/procesing/pipeline.py
+++ experiments/procesing/pipeline.py
@@ -1,15 +1,20 @@
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+
 from extract import DataExtractor
 from mapping import SessionTransitionProbMatrixTransformer, render_graph
+from demand import DemandEstimator
 
+
+# exposable pipelines
+etl_pipeline = Pipeline([
+    ('data_extraction', DataExtractor()),
+])
+pricing_pipeline = Pipeline([
+    ('demand_estimation', DemandEstimator()),
+    ('scaling', StandardScaler()),
+])
 
 if __name__ == "__main__":
-    steps = [
-        ('data_extraction', DataExtractor()),
-        #('transition_matrix', SessionTransitionProbMatrixTransformer(threshold=0.05)),
-    ]
-    pipeline = Pipeline(steps)
-    result = pipeline.fit_transform(None)
-    print(result)
-    print(result.info())
+    processed_data = etl_pipeline.fit_transform(None)
+    pricing = pricing_pipeline.fit_transform(processed_data)
