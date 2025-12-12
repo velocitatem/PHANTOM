35bb6baa1a35c383d4390ea85c019ffb3ec2abbb
Daniel Rosel <daniel@alves.world>
2025-12-12 13:13:05 +0100

    chore: modules and reqs

---
+++ experiments/ml/__init__.py
@@ -0,0 +1,11 @@
+from .evals import evaluate
+from .arch import (
+    XGBoostAgentClassifier,
+    LightGBMAgentClassifier
+)
+
+__all__ =[
+    'evaluate',
+    'XGBoostAgentClassifier',
+    'LightGBMAgentClassifier'
+]
